fccb75cbcd8fecf9b6e0102b66b0fc5078fa9263
Ayoub Lagnaoui <281191540+ayoublagnaoui5-create@users.noreply.github.com>
2026-09-17 15:06:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Type of the sum like S = 1-2+3-4+5-6 ... -100
+## If u enter another type, the result will be False and incorrect
+
+
 try:
     a = int(input("Enter the first number in your sum : "))
     n = int(input("Enter the last number in your sum : "))
@@ -10,3 +14,5 @@ def sum_calcul(a,n):
         return int(-(n+1)/2)
 print(f"S ={sum_calcul(a,n)}")
 
+
+#Ayoub Lagnaoui XX
